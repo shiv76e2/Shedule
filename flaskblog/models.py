00b511ebb8b894a7b52d1bb41f2678a373a1e1a8
shohei <s.shohei.engineer@gmail.com>
2022-05-05 21:33:31 +0900

    model

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,13 @@
 from flaskblog import db
 from datetime import datetime
+from flask_login import UserMixin
+
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
 class Room(db.Model):
     __tablename__ = "room"
@@ -7,3 +15,4 @@ class Room(db.Model):
     name = db.Column(db.String(30))
     capacity = db.Column(db.Integer)
     update_time = db.Column(db.DateTime, default=datetime.utcnow)
+
