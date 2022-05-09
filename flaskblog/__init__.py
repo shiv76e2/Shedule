d0a2005bc143325f661c1105a9008e77330ca210
shohei <s.shohei.engineer@gmail.com>
2022-05-09 22:08:56 +0900

    routes

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,11 @@
+import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flaskblog.config import Config
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 
 def create_app(config_Class=Config):
@@ -10,8 +13,12 @@ def create_app(config_Class=Config):
     app.config.from_object(config_Class)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+    bcrypt.init_app(app)
+    
+    from flaskblog.users.routes import users
     from flaskblog.main.routes import main
     app.register_blueprint(main)
+    app.register_blueprint(users)
     return app
 
     
