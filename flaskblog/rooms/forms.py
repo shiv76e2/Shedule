98a8757e5d334ce530bd2b00919d09ab187cd659
shohei <s.shohei.engineer@gmail.com>
2022-06-26 15:26:51 +0900

    refs #room

--- flaskblog/rooms/forms.py
+++ flaskblog/rooms/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 class RoomForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     capacity = IntegerField('Capacity', validators=[DataRequired()])
-    organization = SelectField(u'TEAM')
+    organization = SelectField(u'TEAM', validate_choice=False, coerce=str)
     submit = SubmitField('登録する')
     
--- flaskblog/rooms/routes.py
+++ flaskblog/rooms/routes.py
@@ -1,7 +1,8 @@
+from tracemalloc import start
 from flask import redirect, render_template, url_for, flash, request, Blueprint
 from flask_login import current_user
 from flaskblog import db
-from flaskblog.models import Organizations, Resources, OrganizationsUsersBelonging
+from flaskblog.models import Organizations, OrganizationsResourcesOwnership, Resources, OrganizationsUsersBelonging
 from flaskblog.rooms.forms import RoomForm
 
 rooms = Blueprint('rooms', __name__)
@@ -19,18 +20,22 @@ def new_room():
     if form.validate_on_submit():
         #TODO: capacity To SelectedField
         room = Resources(name=form.name.data, capacity=form.capacity.data)
+        #TODO: セッション管理
         db.session.add(room)
+        db.session.commit()
+        ownership = OrganizationsResourcesOwnership(organization_id=form.organization.data, resource_id = room.id)
+        db.session.add(ownership)
         db.session.commit()
         flash('Your room has been created!', 'success')
         return redirect(url_for('rooms.load'))
     #GET
-    #TODO: 部屋の所属を表示
+    #FIXME: 部屋の所属を表示
     belongings = OrganizationsUsersBelonging.query.filter_by(user_id = current_user.id)
     if belongings == None:
         return redirect(url_for('organizations.register'))  
     organization_ids = [belonging.organization_id for belonging in belongings]
     organizations = Organizations.query.filter(Organizations.organization_id.in_(organization_ids))
-    form.organization.choices = [(org.id, org.name) for org in organizations]
+    form.organization.choices = [(org.organization_id, org.name) for org in organizations]
     return render_template('create_room.html', form=form)
 
 @rooms.route('/rooms/delete/<int:room_id>')
