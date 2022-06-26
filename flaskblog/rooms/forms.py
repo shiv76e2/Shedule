c8e28fb241956f9921e9d9bb4fa8ab3263d79cf2
shohei <s.shohei.engineer@gmail.com>
2022-06-26 10:20:24 +0900

    cache reset

--- flaskblog/rooms/forms.py
+++ flaskblog/rooms/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class RoomForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     capacity = IntegerField('Capacity', validators=[DataRequired()])
+    organization = SelectField(u'TEAM')
     submit = SubmitField('登録する')
+    
